361f66b44c613de107bb52e8dd88e621ccc210f6
Greg  Careme <gregor.ten1@icloud.com>
2024-06-18 13:30:51 +0200

    form

---
+++ js/form--validation.js
@@ -0,0 +1,66 @@
+document
+  .getElementById("form__main")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let valid = true;
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const consent = document.getElementById("consent");
+    const nameError = name.nextElementSibling;
+    const emailError = email.nextElementSibling;
+
+    // Reset  styles
+    nameError.textContent = "";
+    emailError.textContent = "";
+    name.parentElement.classList.remove("error");
+    email.parentElement.classList.remove("error");
+    consent.parentElement.classList.remove("error");
+
+    // Validate name
+    if (name.value.length < 2 || name.value.length > 100) {
+      name.parentElement.classList.add("error");
+      nameError.textContent = "Invalid Name";
+      valid = false;
+    }
+
+    // Validate email
+    const emailPattern =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailPattern.test(email.value)) {
+      email.parentElement.classList.add("error");
+      emailError.textContent = "Invalid email address.";
+      valid = false;
+    }
+
+    // Validate consent
+    if (!consent.checked) {
+      consent.parentElement.classList.add("error");
+      consent.parentElement.nextElementSibling.textContent =
+        "You must agree to the consent.";
+      valid = false;
+    }
+
+    // If valid, send form data
+    if (valid) {
+      const formData = {
+        name: name.value,
+        email: email.value,
+        consent: consent.checked,
+      };
+
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          alert("Thank you for submitting your form!");
+        })
+        .catch((error) => console.error("Error:", error));
+    }
+  });
