87ab8d7edc35891ae99cfba526da6ef2db40dfcc
Greg  Careme <gregor.ten1@icloud.com>
2024-06-19 11:27:13 +0200

    scroller

--- js/pop--up.js
+++ js/pop--up.js
@@ -5,45 +5,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const isModalClosed = () => localStorage.getItem("modalClosed") === "true";
 
-  //  open the modal
   const openModal = () => {
     modalContainer.style.display = "flex";
   };
 
-  //close the modal
   const closeModal = () => {
     modalContainer.style.display = "none";
     localStorage.setItem("modalClosed", "true");
   };
 
-  // close button
   closeButton.addEventListener("click", function (event) {
     event.preventDefault();
     closeModal();
   });
 
-  // clicking page
   window.addEventListener("click", function (event) {
     if (event.target === modalContainer) {
       closeModal();
     }
   });
 
-  // ESC key
   window.addEventListener("keydown", function (event) {
     if (event.key === "Escape") {
       closeModal();
     }
   });
 
-  //5 seconds
   setTimeout(function () {
     if (!isModalClosed()) {
       openModal();
     }
   }, 5000);
 
-  //  25%
   window.addEventListener("scroll", function () {
     const scrollPosition = window.scrollY + window.innerHeight;
     const scrollThreshold = document.documentElement.scrollHeight * 0.25;
@@ -52,16 +45,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Form
   popupForm.addEventListener("submit", function (event) {
     event.preventDefault();
     const emailInput = document.getElementById("email__modal");
     const email = emailInput.value;
 
-    // Validate email // send to serer
-    const emailPattern =
+    const emailRegex =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (emailPattern.test(email)) {
+    if (emailRegex.test(email)) {
       fetch("https://jsonplaceholder.typicode.com/posts", {
         method: "POST",
         headers: {
